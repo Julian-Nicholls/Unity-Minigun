bfc16fdbccc93c3e947505a6b685bb372c78645c
Julian Nicholls <jnicholls95@outlook.com>
2017-07-26 12:44:45 -0400

    Tinkered with AI
    tried to make the enemies move more appropriately

--- Minigun/Assets/Scripts/EnemyScript.cs
+++ Minigun/Assets/Scripts/EnemyScript.cs
@@ -12,44 +12,79 @@ public class EnemyScript : MonoBehaviour {
 	int strategies = 0;
 	int strategy;
 	float elapsedTime = 0;
-	bool balanced;
+	bool gravity = false;
 	Transform parentTransform;
 
 	public float getUpSpeed;
+	public float restandHeight;
 	public float speed;
 	public float minDistance;
 	public GameObject minigun;
+	public GameObject capsule;
 
 	public HealthKeeper healthKeeper;
 	public ScoreKeeper scoreKeeper;
+	Rigidbody rb;
+
+	bool coroutineRunning;
+	bool fallOrFly;
+
+	public float hoverMultiplier;
 
 	// Use this for initialization
 	void Start () {
-		renderer = GetComponent<MeshRenderer> ();
+		renderer = capsule.GetComponent<MeshRenderer> ();
 		renderer.material = enemyMaterials [health];
 		transform.LookAt (minigun.transform);
 		scoreKeeper = FindObjectOfType<ScoreKeeper> ();
 		healthKeeper = FindObjectOfType<HealthKeeper> ();
+		rb = GetComponent<Rigidbody> ();
 
-		parentTransform = GetComponentInParent<Transform> ();
+
+
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		elapsedTime = elapsedTime + Time.deltaTime;
+	void FixedUpdate () {
+		
+		elapsedTime = elapsedTime + Time.fixedDeltaTime;
 
 		if (elapsedTime > strategyTimer) {
 			strategy = Random.Range (0, strategies);
-			parentTransform.LookAt (minigun.transform);
+			transform.LookAt (minigun.transform);
 			elapsedTime = 0;
 		}
 
-		if (Vector3.Distance (parentTransform.position, minigun.transform.position) > minDistance) {
-			
+
+
+		if (Vector3.Distance (transform.position, minigun.transform.position) > minDistance) {
+
+			//transform.Translate (Vector3.forward * Time.fixedDeltaTime * speed);
+			if (rb.useGravity && fallOrFly) {
+
+
+			} else if (rb.useGravity && !fallOrFly) {
+				rb.AddForce (-Physics.gravity*2, ForceMode.Acceleration);
+				rb.AddForce (transform.forward * speed * Time.fixedDeltaTime, ForceMode.Impulse);
+
+			} else if (!rb.useGravity) {
+
+				if (transform.position.y < 0) {
+					rb.AddForce(-Physics.gravity * Time.deltaTime * Mathf.Abs(0 - transform.position.y), ForceMode.Acceleration);
+				}
+				if (transform.position.y > 0) {
+					rb.AddForce(Physics.gravity * Time.deltaTime * Mathf.Abs(0 - transform.position.y), ForceMode.Acceleration);
+				}
+
+				rb.AddForce (transform.forward * speed * Time.fixedDeltaTime, ForceMode.Impulse);
+
+			}
+
 			if (strategy == 0) {
 				// straight ahead
 
-				parentTransform.Translate (Vector3.forward * Time.deltaTime * speed);
+
 			}
 			if (strategy == 1) {
 				//wide flank
@@ -59,16 +94,8 @@ public class EnemyScript : MonoBehaviour {
 			if (strategy == 2) {
 				//narrow flank
 
-
+				//rot.eulerAngles * getUpSpeed * Time.deltaTime
 			}
-		}
-
-
-		if (transform.up != Vector3.up) {
-			
-			var rot = Quaternion.FromToRotation (transform.up, Vector3.up);
-			GetComponent<Rigidbody> ().AddTorque (new Vector3 (rot.x, rot.y, rot.z) * getUpSpeed);
-
 		} else {
 			
 		}
@@ -95,13 +122,44 @@ public class EnemyScript : MonoBehaviour {
 
 	void OnCollisionEnter(Collision collision){
 		if (collision.gameObject.tag == "Bullet") {
-			takeDamage ();
+			
+			//rb.isKinematic = false;
 			Destroy(collision.gameObject);
+			takeDamage ();
+
+			if (!coroutineRunning) {
+				StartCoroutine (bulletCoroutine ());
+			}
 		}
 
 		if (collision.gameObject.tag == "Player") {
 			healthKeeper.SendMessage("takeDamage");	
 		}
+	}
+
+	IEnumerator bulletCoroutine(){
+		coroutineRunning = true;
+		if (Random.value > 0.5) {
+			fallOrFly = true;
+		} else {
+			fallOrFly = false;
+		}
+
+		rb.useGravity = false;
+		yield return new WaitForSeconds (getUpSpeed);
+		rb.useGravity = true;
+
+		//rb.isKinematic = true;
+
+		//setPosAndRot ();
+
+		coroutineRunning = false;
+		StopCoroutine (bulletCoroutine());
+	}
+
+	void setPosAndRot(){
+		var rot = Quaternion.FromToRotation (transform.up, Vector3.up);
+		transform.SetPositionAndRotation(transform.position + new Vector3(0, restandHeight, -2f), rot);
 	}
 
 }
