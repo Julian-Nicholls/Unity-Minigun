f1ab6aee8eaee975a64ce374d3e1161625c4fd26
Julian Nicholls <jnicholls95@outlook.com>
2017-07-31 10:23:35 -0400

    Random Commit
    I don't remeber the changes but i want them backed up

--- Minigun/Assets/Scripts/EnemyScript.cs
+++ Minigun/Assets/Scripts/EnemyScript.cs
@@ -24,6 +24,7 @@ public class EnemyScript : MonoBehaviour {
 
 	public HealthKeeper healthKeeper;
 	public ScoreKeeper scoreKeeper;
+	public Hover hover;
 	Rigidbody rb;
 
 	bool coroutineRunning;
@@ -47,58 +48,7 @@ public class EnemyScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		
-		elapsedTime = elapsedTime + Time.fixedDeltaTime;
 
-		if (elapsedTime > strategyTimer) {
-			strategy = Random.Range (0, strategies);
-			transform.LookAt (minigun.transform);
-			elapsedTime = 0;
-		}
-
-
-
-		if (Vector3.Distance (transform.position, minigun.transform.position) > minDistance) {
-
-			//transform.Translate (Vector3.forward * Time.fixedDeltaTime * speed);
-			if (rb.useGravity && fallOrFly) {
-
-
-			} else if (rb.useGravity && !fallOrFly) {
-				rb.AddForce (-Physics.gravity*2, ForceMode.Acceleration);
-				rb.AddForce (transform.forward * speed * Time.fixedDeltaTime, ForceMode.Impulse);
-
-			} else if (!rb.useGravity) {
-
-				if (transform.position.y < 0) {
-					rb.AddForce(-Physics.gravity * Time.deltaTime * Mathf.Abs(0 - transform.position.y), ForceMode.Acceleration);
-				}
-				if (transform.position.y > 0) {
-					rb.AddForce(Physics.gravity * Time.deltaTime * Mathf.Abs(0 - transform.position.y), ForceMode.Acceleration);
-				}
-
-				rb.AddForce (transform.forward * speed * Time.fixedDeltaTime, ForceMode.Impulse);
-
-			}
-
-			if (strategy == 0) {
-				// straight ahead
-
-
-			}
-			if (strategy == 1) {
-				//wide flank
-
-
-			}
-			if (strategy == 2) {
-				//narrow flank
-
-				//rot.eulerAngles * getUpSpeed * Time.deltaTime
-			}
-		} else {
-			
-		}
 	}
 
 	void takeDamage(){
@@ -145,9 +95,12 @@ public class EnemyScript : MonoBehaviour {
 			fallOrFly = false;
 		}
 
-		rb.useGravity = false;
+		hover.changeMode ();
+
 		yield return new WaitForSeconds (getUpSpeed);
-		rb.useGravity = true;
+		rb.useGravity = false;
+
+		Debug.Log ("Coroutine executed");
 
 		//rb.isKinematic = true;
 
