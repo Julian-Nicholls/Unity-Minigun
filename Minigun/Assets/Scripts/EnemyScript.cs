9be70d1c9da37ed920bedaff324619e1a7638fe4
Julian Nicholls <jnicholls95@outlook.com>
2017-07-31 11:29:05 -0400

    Refactored Code
    And worked on Enemy Movement

--- Minigun/Assets/Scripts/EnemyScript.cs
+++ Minigun/Assets/Scripts/EnemyScript.cs
@@ -8,29 +8,15 @@ public class EnemyScript : MonoBehaviour {
 	public Material[] enemyMaterials;
 	MeshRenderer renderer;
 
-	public float strategyTimer;
-	int strategies = 0;
-	int strategy;
-	float elapsedTime = 0;
-	bool gravity = false;
 	Transform parentTransform;
 
-	public float getUpSpeed;
-	public float restandHeight;
-	public float speed;
-	public float minDistance;
 	public GameObject minigun;
 	public GameObject capsule;
 
-	public HealthKeeper healthKeeper;
 	public ScoreKeeper scoreKeeper;
-	public Hover hover;
-	Rigidbody rb;
 
-	bool coroutineRunning;
-	bool fallOrFly;
 
-	public float hoverMultiplier;
+	public EnemyMovement enemyMovement;
 
 	// Use this for initialization
 	void Start () {
@@ -38,12 +24,7 @@ public class EnemyScript : MonoBehaviour {
 		renderer.material = enemyMaterials [health];
 		transform.LookAt (minigun.transform);
 		scoreKeeper = FindObjectOfType<ScoreKeeper> ();
-		healthKeeper = FindObjectOfType<HealthKeeper> ();
-		rb = GetComponent<Rigidbody> ();
-
-
-
-
+		enemyMovement = GetComponent<EnemyMovement> ();
 	}
 	
 	// Update is called once per frame
@@ -51,7 +32,7 @@ public class EnemyScript : MonoBehaviour {
 
 	}
 
-	void takeDamage(){
+	public void takeDamage(){
 		health--;
 
 		if (health < 0) {
@@ -68,51 +49,6 @@ public class EnemyScript : MonoBehaviour {
 
 	public void setMinigun(GameObject mg){
 		minigun = mg;
-	}
-
-	void OnCollisionEnter(Collision collision){
-		if (collision.gameObject.tag == "Bullet") {
-			
-			//rb.isKinematic = false;
-			Destroy(collision.gameObject);
-			takeDamage ();
-
-			if (!coroutineRunning) {
-				StartCoroutine (bulletCoroutine ());
-			}
-		}
-
-		if (collision.gameObject.tag == "Player") {
-			healthKeeper.SendMessage("takeDamage");	
-		}
-	}
-
-	IEnumerator bulletCoroutine(){
-		coroutineRunning = true;
-		if (Random.value > 0.5) {
-			fallOrFly = true;
-		} else {
-			fallOrFly = false;
-		}
-
-		hover.changeMode ();
-
-		yield return new WaitForSeconds (getUpSpeed);
-		rb.useGravity = false;
-
-		Debug.Log ("Coroutine executed");
-
-		//rb.isKinematic = true;
-
-		//setPosAndRot ();
-
-		coroutineRunning = false;
-		StopCoroutine (bulletCoroutine());
-	}
-
-	void setPosAndRot(){
-		var rot = Quaternion.FromToRotation (transform.up, Vector3.up);
-		transform.SetPositionAndRotation(transform.position + new Vector3(0, restandHeight, -2f), rot);
 	}
 
 }
